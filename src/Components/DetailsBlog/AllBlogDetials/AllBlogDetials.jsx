6dec4990084dc0c2d9533a2592c38c94fdcf5ad5
jahid777 <za0162hid@gmail.com>
2025-01-15 18:45:54 +0000

    blogs

--- src/Components/DetailsBlog/AllBlogDetials/AllBlogDetials.jsx
+++ src/Components/DetailsBlog/AllBlogDetials/AllBlogDetials.jsx
@@ -1,10 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import { useParams } from 'react-router-dom';
 
 const AllBlogDetials = () => {
+    const [blogDetails, setBlogDetails] = useState(null);
+    const { id } = useParams();
+
+    useEffect(() => {
+        const fetchBlogDetails = async () => {
+            try {
+                const response = await axios.get(`/api/blogs/${id}`);
+                setBlogDetails(response.data.data);
+            } catch (error) {
+                console.error('Error fetching blog details:', error);
+            }
+        };
+
+        fetchBlogDetails();
+    }, [id]);
+
+    if (!blogDetails) return <div>Loading...</div>;
+    
     return (
         <div>
-            blog with parames by id
-        </div>
+        <h1>{blogDetails.title}</h1>
+        <p>{blogDetails.shortDescription}</p>
+        <img src={blogDetails.thumbnail} alt={blogDetails.title} />
+        <p>Department: {blogDetails.department}</p>
+        <p>Created At: {new Date(blogDetails.createdAt).toLocaleDateString()}</p>
+    </div>
     );
 };
 
